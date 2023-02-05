d2afcec1c2df3679a4b510901bd246c2b54f7590
unknown <rohitgeddam2018@gmail.com>
2023-02-04 23:28:08 -0500

    cityMap

--- src/module3/EarthquakeCityMap.java
+++ src/module3/EarthquakeCityMap.java
@@ -24,7 +24,7 @@ import parsing.ParseFeed;
 /** EarthquakeCityMap
  * An application with an interactive map displaying earthquake data.
  * Author: UC San Diego Intermediate Software Development MOOC team
- * @author Rohit Geddam
+ * @author Rohit Geddam 
  * Date: July 17, 2015
  * */
 public class EarthquakeCityMap extends PApplet {
